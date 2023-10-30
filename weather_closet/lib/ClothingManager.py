f7dce23f256edb25526ba0fa78db727ccc763e23
ScientistaMado <profesorsamuelcortes@gmail.com>
2023-10-30 12:34:47 -0300

    ClothingManagaer lib incluye ahora la función suggestOutfit

--- weather_closet/lib/ClothingManager.py
+++ weather_closet/lib/ClothingManager.py
@@ -1,9 +1,10 @@
 from machine import Pin
 
+
 class ClothingManager():
     def __init__(self, data_pin, clock_pin, latch_pin):
         self.data_pin = data_pin
-        self.clock_pin = clock_pin 
+        self.clock_pin = clock_pin
         self.latch_pin = latch_pin
         self.clothes = [0] * 14
         self.clothing_mapping = {
@@ -32,7 +33,7 @@ class ClothingManager():
             else:
                 print(f"Prenda no válida: {clothe}")
         return self.clothes
-    
+
     def turnOnLed(self):
         self.latch_pin.value(0)
 
@@ -40,5 +41,34 @@ class ClothingManager():
             self.clock_pin.value(0)
             self.data_pin.value(status_led)
             self.clock_pin.value(1)
-            
+
         self.latch_pin(1)
+
+    def suggestOutfit(self, weather_info):
+
+        clothes_suggest = {
+            1: ["TShirt", "Shorts", "Sandals", "Sneakers"],
+            2: ["TShirt", "Pants", "Sneakers", "Vest"],
+            3: ["TShirt", "Pants", "Sneakers", "Jacket"],
+            4: ["TShirt", "Pants", "Sneakers", "Jacket", "Hoodie"],
+            5: ["TShirt", "Pants", "Sneakers", "Jacket", "Parka"],
+            6: ["TShirt", "Pants", "Boots", "Jacket", "Parka"],
+            7: ["TShirt", "Pants", "Boots", "Hoodie", "Parka"],
+            8: ["TShirt", "Pants", "Boots", "Hoodie", "Jacket", "Parka"],
+            9: ["TShirt", "Pants", "Boots", "Hoodie", "Jacket", "Parka"],
+            10: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            11: ["TShirt", "Pants", "Boots", "Jacket", "Parka"],
+            12: ["TShirt", "Pants", "Boots", "Jacket", "Parka"],
+            13: ["TShirt", "Pants", "Boots", "Jacket", "Parka"],
+            14: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            15: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            16: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            17: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            18: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            19: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            20: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            21: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+            22: ["TShirt", "Pants", "AnkleBoots", "Hoodie", "Jacket", "Parka"],
+        }
+        self.wear(clothes_suggest[weather_info["Símbolo del tiempo"]])
+        self.turnOnLed()
--- weather_closet/weather_closet.py
+++ weather_closet/weather_closet.py
@@ -19,119 +19,91 @@ URL_API = 'YOUR URL API'
 
 
 def connectWifi(ssid, password):
-    
+
     station = network.WLAN(network.STA_IF)
     station.active(True)
-    station.connect(ssid,password)
-    
-    oled.text("Conectando", 10,10)
+    station.connect(ssid, password)
+
+    oled.text("Conectando", 10, 10)
     oled.show()
     dot = 10
-    
+
     while not station.isconnected():
         print('conectando')
-        oled.text(".", dot+6,10)
+        oled.text(".", dot+6, 10)
         oled.show()
         time.sleep(1)
 
     print(f'Conectado con éxito a {ssid}')
-    
+
     oled.fill(0)
-    oled.text("Conectado", 10,10)
+    oled.text("Conectado", 10, 10)
     oled.show()
 
 
 def fetchApi(api_url, timeout=10):
     try:
         response = requests.get(api_url, timeout=timeout)
-        
+
         if response.status_code == 200:
             return response.text
         else:
-            print('Error en la solicitud. Código de respuesta HTTP:', response.status_code)
+            print('Error en la solicitud. Código de respuesta HTTP:',
+                  response.status_code)
             return None
-        
+
     except Exception as e:
         print('Error en la solicitud:', str(e))
         return None
-    
+
 
 def getVar(report, tag_element):
-    
-        start = report.find(tag_element)
-        
-        if tag == "Símbolo del tiempo":
-            var = re.search(r'id="(.*?)"', report[start:])
-        else:
-            var = re.search(r'value="(.*?)"', report[start:])
-            
-        return var.group(1)
+
+    start = report.find(tag_element)
+
+    if tag == "Símbolo del tiempo":
+        var = re.search(r'id="(.*?)"', report[start:])
+    else:
+        var = re.search(r'value="(.*?)"', report[start:])
+
+    return var.group(1)
 
 
 def openIcon(icon_id):
-    
+
     with open(f'icons/{icon_id}.pbm', "rb") as file:
         file.readline()
         xy = file.readline()
         x = int(xy.split()[0])
         y = int(xy.split()[1])
         icon = bytearray(file.read())
-    
+
     return framebuf.FrameBuffer(icon, x, y, framebuf.MONO_HLSB)
 
 
 def showInOled(data):
     oled.fill(0)
-    
+
     day = data["Día"]
     day_x = int((128 - (len(day)*8))/2)
-    
+
     if day == "Miércoles":
         day = "Miercoles"
     if day == "Sábado":
         day = "Sábado"
-    
-    oled.text(day, day_x,2)
-    
+
+    oled.text(day, day_x, 2)
+
     t = f'{data["Temperatura Mínima"]}-{data["Temperatura Máxima"]}C'
     t_x = int((128 - (len(t)*8))/2)
 
     oled.text(t, t_x, 18)
 
-    
     icon_id = data["Símbolo del tiempo"]
     oled.blit(openIcon(icon_id), 48, 32)
-    
+
     oled.show()
 
-def suggestOutfit(manager, id_weather):
-    clothes_suggest = {
-        1 : ["TShirt", "Shorts", "Sandals", "Sneakers"]
-        2 : ["TShirt", "Pants", "Sneakers", "Vest"]
-        3 : ["TShirt", "Pants", "Sneakers", "Jacket"]
-        4 : []
-        5 : []
-        6 : []  
-        7 : []
-        8 : []
-        9 : []
-        10 : []
-        11 : []
-        12 : []
-        13 : []
-        14 : []
-        15 : []
-        16 : []
-        17 : []
-        18 : []
-        19 : []
-        20 : []
-        21 : []
-        22 : []
-    }
-    manager.wear(id_weather)
-    manager.turnOnLed()
-    
 
 i2c = I2C(scl=Pin(5), sda=Pin(4), freq=400000)
 oled = sh1106.SH1106_I2C(128, 64, i2c, Pin(16), 0x3c)
@@ -148,7 +120,7 @@ data_dict = {"Temperatura Mínima": "",
 
 for tag in data_dict:
     data_dict[tag] = getVar(data_api, tag)
-    
+
 showInOled(data_dict)
 print(data_dict)
 
@@ -158,22 +130,4 @@ latch_pin = Pin(14, Pin.OUT)
 
 clothing_manager = ClothingManager(data_pin, clk_pin, latch_pin)
 
-suggestOutfit(manager, data_dict["Símbolo del tiempo"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+clothing_manager.suggestOutfit(data_dict)
